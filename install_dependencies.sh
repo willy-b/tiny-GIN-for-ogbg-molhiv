bb7dd5e102082224ec0fa1e404ddc607c4d886b5
William Bruns <adde.animulis@gmail.com>
2025-03-21 14:24:16 -0400

    for https://github.com/willy-b/tiny-GIN-for-ogbg-molhiv/issues/9 pin older dependencies until OGB team repo is ready for pytorch 2.6

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+pip install torch==2.5.1
 pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.2.1+cu121.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-2.2.1+cu121.html
-pip install torch-geometric
-pip install ogb
+pip install torch-geometric==2.5.3
+pip install ogb==1.3.6
